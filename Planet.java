882ce14bb400b80bcfe6b87e3fc2b8ba56021b18
dmin01 <pelicanlaser@gmail.com>
2015-03-29 01:37:26 -0400

    Update Planet.java

--- Planet.java
+++ Planet.java
@@ -2,23 +2,51 @@ package hackBCA;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-
-import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 public class Planet extends JPanel {
-	private static final int LENGTH = 30;
-	private static final int WIDTH = 30;
+	private int length = 30;
+	private int width = 30;
+	private int x, xa = 1;
+	private int y, ya = 1;
+	private SystemPanel game;
 
 	public Planet(SystemPanel sys) {
-		
+		game = sys;
+		x = game.getWidth() / 2 - 20;
+		y = game.getHeight() / 2 - 20;
+	}
+	
+	public Planet(SystemPanel sys, int newL, int newW) {
+		game = sys;
+		x = game.getWidth() / 2 - 20;
+		y = game.getHeight() / 2 - 20;
+		length = newL;
+		width = newW;
+	}
+	
+	public Planet(SystemPanel sys, int newL, int newW, int newX, int newY) {
+		game = sys;
+		x = newX;
+		y = newY;
+		length = newL;
+		width = newW;
 	}
 	
 	public void paintComponent(Graphics g) {
         g.setColor(Color.ORANGE);
-        g.fillOval(200, 150, LENGTH, WIDTH);
+        g.fillOval(x, y, length, width);super.paintComponent(g); 
 	}
 	
+	void update() {
+		x += xa;
+		y += ya;
+		System.out.println(x + " " + y);
+	}
+	
+	private void fall() {
+		y -= ya;
+	}
 }
 
